ef6ec04ddd860f4019469c8731679630912d14e4
Daniil Zadorozhnyy <zadd01@vse.cz>
2022-04-07 21:12:34 +0200

    Create feed item model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  FeedItem
+//
+//  Created by Daniil Zadorozhnyy on 07.04.2022.
+//
+
+import Foundation
+
+struct FeedItem {
+    var id: UUID
+    var description: String?
+    var location: String?
+    var url: URL
+}
