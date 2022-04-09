e38d99501cd7b4019040bc6e3dd0b05d6590bb91
Daniil Zadorozhnyy <zadd01@vse.cz>
2022-04-09 19:54:20 +0200

    Delivers error on 200 HTTPResponse with invalid data

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -51,7 +51,6 @@ class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversErrorOnNot200Response() {
         let (sut, client) = makeSUT()
         
-        
         let samples = [199, 201, 300, 400, 500].enumerated()
                 
         samples.forEach { index,  code in
@@ -63,8 +62,21 @@ class RemoteFeedLoaderTests: XCTestCase {
             XCTAssertEqual(capturedErrors, [.invalidData])
         }
     }
-    // MARK: - Helpers
     
+    func test_load_deliversErrorOn200ResponseWithInvalidJSON() {
+        let (sut, client) = makeSUT()
+        
+        var capturedErrors = [RemoteFeedLoader.Error]()
+        sut.load { capturedErrors.append($0) }
+        
+        let invalidJSON = Data(base64Encoded: "")!
+        client.complete(withStatusCode: 200, data: invalidJSON)
+        
+        XCTAssertEqual(capturedErrors, [.invalidData])
+        
+    }
+    
+    // MARK: - Helpers
     func makeSUT(url: URL = URL(string: "https://a-given-url.com")!) -> (RemoteFeedLoader, HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(url: url, client: client)
@@ -86,7 +98,11 @@ class RemoteFeedLoaderTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode code: Int, at index: Int = 0) {
+        func complete(
+            withStatusCode code: Int,
+            data: Data = Data(),
+            at index: Int = 0
+        ) {
             let response = HTTPURLResponse(
                 url: requestedURLs[index],
                 statusCode: code,
@@ -94,7 +110,11 @@ class RemoteFeedLoaderTests: XCTestCase {
                 headerFields: nil
             )!
             
-            messages[index].completion(.success(response))
+            messages[index].completion(.success(data, response))
+        }
+        
+        func complete(withStatusCode code: Int) {
+             
         }
     }
 }
