32c94a6e25f8ee0d3801899e80705f41c9008d5a
Daniil Zadorozhnyy <zadd01@vse.cz>
2022-04-10 23:33:39 +0200

    Replace RemoteFeedLoader Result enum with the predefined in the FeedLoader Result enum to conform to FeedLoader

--- EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public final class RemoteFeedLoader {
+public final class RemoteFeedLoader: FeedLoader {
     private let url: URL
     private let client: HTTPClient
     
@@ -16,10 +16,7 @@ public final class RemoteFeedLoader {
         case invalidData
     }
     
-    public enum Result: Equatable {
-        case success([FeedItem])
-        case failure(Error)
-    }
+    public typealias Result = LoadFeedResult<Error>
     
     public init(url: URL = URL(string: "https://a-url.com")!, client: HTTPClient) {
         self.url = url
@@ -28,7 +25,7 @@ public final class RemoteFeedLoader {
     
     public func load(completion: @escaping (Result) -> Void) {
         client.get(from: url) { [weak self] result in
-            guard self != nil else { return } 
+            guard self != nil else {  return } 
             switch result {
             case let .success(data, response):
                 completion(FeedItemsMapper.map(data, response))
