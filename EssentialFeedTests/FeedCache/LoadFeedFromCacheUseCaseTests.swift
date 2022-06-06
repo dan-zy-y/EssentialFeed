ec1e50a01a748d1968343078f34d015a6bb1951d
Daniil Zadorozhnyy <zadd01@vse.cz>
2022-06-06 23:16:22 +0200

    Load command requests cache retrieval

--- EssentialFeedTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
@@ -15,6 +15,15 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         
         XCTAssertEqual(store.receivedMessages, [])
     }
+    
+    func test_load_requestsCacheRetrieval() {
+        
+        let (sut, store) = makeSUT()
+        
+        sut.load()
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
 
     private func makeSUT(
         currentDate: @escaping () -> Date = Date.init,
