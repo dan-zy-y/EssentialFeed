2ead5d8c55a0bb052b541baabe1ed5b8f938f03d
Daniil Zadorozhnyy <zadd01@vse.cz>
2022-08-24 21:32:19 +0200

    Merge `FeedRefreshViewController` with `FeedViewController` since the `UIRefreshControl` creation and configuration now lives in the storyboard

--- EssentialFeed/EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
@@ -18,12 +18,11 @@ public final class FeedUIComposer {
         let bundle = Bundle(for: FeedViewController.self)
         let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
         let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
-        let refreshController = feedController.refreshController!
-        refreshController.delegate = presentationAdapter
+        feedController.delegate = presentationAdapter
         
         presentationAdapter.presenter = FeedPresenter(
             feedView: FeedViewAdapter(controller: feedController, imageLoader: imageLoader),
-            loadingView: WeakRefVirtualProxy(refreshController)
+            loadingView: WeakRefVirtualProxy(feedController)
         )
         
         return feedController
@@ -76,7 +75,7 @@ private final class FeedViewAdapter: FeedView {
     }
 }
 
-private final class FeedLoaderPresentationAdapter: FeedRefreshViewControllerDelegate {
+private final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
     var presenter: FeedPresenter?
     private let feedLoader: FeedLoader
     
