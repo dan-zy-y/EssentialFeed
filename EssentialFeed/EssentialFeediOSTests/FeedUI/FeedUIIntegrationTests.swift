068ea02bcfdf010772bd0d33e9a633bea9dcbde5
Daniil Zadorozhnyy <zadd01@vse.cz>
2022-08-25 22:07:43 +0200

    Dispatch background feed completion to main thread before updating the UI since UIKit is not thread safe

--- EssentialFeed/EssentialFeediOSTests/FeedUI/FeedUIIntegrationTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedUI/FeedUIIntegrationTests.swift
@@ -266,6 +266,19 @@ final class FeedUIIntegrationTests: XCTestCase {
         XCTAssertNil(view?.renderedImage, "Expected no rendered image when image load finishes after the view is not visbile anymore")
     }
     
+    func test_loadFeedCompletion_dispatchesFromBackgroundToMainThread() {
+        let (sut, loader) = makeSUT()
+        sut.loadViewIfNeeded()
+        
+        let exp = expectation(description: "Wait for background queue")
+        DispatchQueue.global().async {
+            loader.completeFeedLoading()
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1)
+    }
+    
     // MARK: - Helpers
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (FeedViewController, LoaderSpy) {
         let loader = LoaderSpy()
