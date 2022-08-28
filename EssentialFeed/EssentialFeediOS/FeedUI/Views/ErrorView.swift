aa2bc2c6d46d1871ec2719bb76796fed484ec992
Daniil Zadorozhnyy <zadd01@vse.cz>
2022-08-28 13:22:17 +0200

    Move `ErrorView` to a separate file

---
+++ EssentialFeed/EssentialFeediOS/FeedUI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by Daniil Zadorozhnyy on 28.08.2022.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
