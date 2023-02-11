8385eb6802b7e61b24057e04120d4361cab18da7
Daniil Zadorozhnyy <zadd01@vse.cz>
2023-02-11 20:18:03 +0100

    Test FeedItemsMapper in isolation

--- EssentialFeed/EssentialFeed/FeedAPI/FeedItemsMapper.swift
+++ EssentialFeed/EssentialFeed/FeedAPI/FeedItemsMapper.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-internal final class FeedItemsMapper {
+public final class FeedItemsMapper {
     private struct Root: Decodable {
         private struct RemoteFeedItem: Decodable {
             var id: UUID
@@ -39,7 +39,7 @@ internal final class FeedItemsMapper {
         }
     }
     
-    internal static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedImage] {
+    public static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedImage] {
         guard response.isOK,
               let root = try? JSONDecoder().decode(Root.self, from: data) else {
             throw RemoteFeedLoader.Error.invalidData
---
+++ EssentialFeed/EssentialFeedTests/FeedAPI/FeedItemsMapperTests.swift
@@ -0,0 +1,85 @@
+//
+//  FeedItemsMapperTests.swift
+//  EssentialFeedTests
+//
+//  Created by Daniil Zadorozhnyy on 08.04.2022.
+//
+
+import XCTest
+import EssentialFeed
+
+class FeedItemsMapperTests: XCTestCase {
+    func test_map_throwsErrorOnNot200Response() throws {
+        let samples = [199, 201, 300, 400, 500]
+                
+        let validJSON = makeItemsJSON([])
+        try samples.forEach { code in
+            XCTAssertThrowsError(
+                try FeedItemsMapper.map(validJSON, HTTPURLResponse(statusCode: code))
+            )
+        }
+    }
+    
+    func test_map_throwsErrorOn200ResponseWithInvalidJSON() {
+        let invalidJSON = Data(base64Encoded: "")!
+        
+        XCTAssertThrowsError(
+            try FeedItemsMapper.map(invalidJSON, HTTPURLResponse(statusCode: 200))
+        )
+    }
+    
+    func test_map_deliversNoItemsOn200HTTPResponseWithEmptyList() throws {
+        let emptyJSON = makeItemsJSON([])
+        
+        let result = try FeedItemsMapper.map(emptyJSON, HTTPURLResponse(statusCode: 200))
+        XCTAssertEqual(result, [])
+    }
+    
+    func test_map_deliversItemsOn200HTTPResponseWithJSONList() throws {
+        let item1 = makeItem(id: UUID(), imageURL: URL(string: "https//:a-url.com")!)
+        
+        let item2 = makeItem(
+            id: UUID(),
+            description: "a description",
+            location: "a location",
+            imageURL: URL(string: "https//:a-url.com")!
+        )
+            
+        let items = [item1.model, item2.model]
+        let json = makeItemsJSON([item1.json, item2.json])
+        let result = try FeedItemsMapper.map(json, HTTPURLResponse(statusCode: 200))
+        XCTAssertEqual(result, items)
+    }
+    
+    private func makeItem(
+        id: UUID,
+        description: String? = nil,
+        location: String? = nil,
+        imageURL: URL
+    ) -> (model: FeedImage, json: [String: Any]) {
+        let item = FeedImage(id: id, description: description, location: location, url: imageURL)
+        
+        let json = [
+            "id": id.uuidString,
+            "description": description,
+            "location": location,
+            "image": imageURL.absoluteString
+        ].compactMapValues { $0 } 
+        
+        return (item, json)
+    }
+    
+    private func makeItemsJSON(_ items: [[String: Any]]) -> Data {
+        let itemsJSON = [
+            "items": items
+        ]
+        let json = try! JSONSerialization.data(withJSONObject: itemsJSON)
+        return json
+    }
+}
+
+extension HTTPURLResponse {
+    convenience init(statusCode: Int) {
+        self.init(url: anyURL(), statusCode: statusCode, httpVersion: nil, headerFields: nil)!
+    }
+}
