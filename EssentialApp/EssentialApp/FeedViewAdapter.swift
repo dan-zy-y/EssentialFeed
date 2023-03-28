4c6745342bfb66b74fd14c7eef203b0123437c1d
Daniil Zadorozhnyy <dz@ditio.no>
2023-03-28 23:33:15 +0200

    Remove unused FeedImagePresenter logic

--- EssentialApp/EssentialApp/FeedViewAdapter.swift
+++ EssentialApp/EssentialApp/FeedViewAdapter.swift
@@ -27,7 +27,7 @@ final class FeedViewAdapter: ResourceView {
         controller?.display(viewModel.feed.map { model in
             let adapter = ResourceLoaderPresentationAdapter<Data, WeakRefVirtualProxy<FeedImageCellController>>(loader: { [imageLoader] in  imageLoader(model.url) })
             let view = FeedImageCellController(
-                viewModel: FeedImagePresenter<FeedImageCellController, UIImage>.map(model),
+                viewModel: FeedImagePresenter.map(model),
                 delegate: adapter
             )
             
