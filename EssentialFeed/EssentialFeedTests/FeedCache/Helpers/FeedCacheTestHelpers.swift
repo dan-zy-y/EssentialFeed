7942683211bfac5eda6d6b50f5c0409c96dd460c
Daniil Zadorozhnyy <dz@ditio.no>
2023-03-29 20:33:01 +0200

    Map ImageComments into ImageCommentsViewModel

--- EssentialFeed/EssentialFeedTests/FeedCache/Helpers/FeedCacheTestHelpers.swift
+++ EssentialFeed/EssentialFeedTests/FeedCache/Helpers/FeedCacheTestHelpers.swift
@@ -31,13 +31,5 @@ extension Date {
         return 7
     }
     
-    private func adding(days: Int) -> Date {
-        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
-    }
-}
-
-extension Date {
-    func adding(seconds: Double) -> Date {
-        return self + seconds
-    }
+    
 }
