c7f6e3e63bb0fb3a869a17609b69e7e4b652f796
Daniil Zadorozhnyy <dz@ditio.no>
2023-04-03 21:27:47 +0200

    Implement Image Comments UI

---
+++ EssentialFeed/EssentialFeediOS/ImageCommentsUI/Views/ImageCommentCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCommentCell.swift
+//  EssentialFeediOS
+//
+//  Created by Daniil Zadorozhnyy on 03.04.2023.
+//
+
+import UIKit
+
+public final class ImageCommentCell: UITableViewCell {
+    @IBOutlet private (set) public var messageLabel: UILabel!
+    @IBOutlet private (set) public var usernameLabel: UILabel!
+    @IBOutlet private (set) public var dateLabel: UILabel!
+}
---
+++ EssentialFeed/EssentialFeediOSTests/ImageCommentsUI/ImageCommentsSnapshotTests.swift
@@ -0,0 +1,58 @@
+//
+//  ImageCommentsSnapshotTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Daniil Zadorozhnyy on 02.04.2023.
+//
+
+import XCTest
+import EssentialFeediOS
+@testable import EssentialFeed
+
+final class ImageCommentsSnapshotTests: XCTestCase {
+    func test_listWithContent() {
+        let sut = makeSUT()
+        
+        sut.display(comments())
+        
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "IMAGE_COMMENTS_light")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "IMAGE_COMMENTS_dark")
+    }
+    
+    private func makeSUT() -> ListViewController {
+        let bundle = Bundle(for: ListViewController.self)
+        let storyboard = UIStoryboard(name: "ImageComments", bundle: bundle)
+        let controller = storyboard.instantiateInitialViewController() as! ListViewController
+        controller.loadViewIfNeeded()
+        controller.tableView.showsVerticalScrollIndicator = false
+        controller.tableView.showsHorizontalScrollIndicator = false
+        
+        return controller
+    }
+    
+    private func comments() -> [CellController] {
+        return [
+            ImageCommentCellController(
+                model: ImageCommentViewModel(
+                    message: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series or murals painted directIv on a 1,316 m long remnant or the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
+                    date: "1000 years ago",
+                    username: "a long long long long username"
+                )
+            ),
+            ImageCommentCellController(
+                model: ImageCommentViewModel(
+                    message: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North, Wales.",
+                    date: "10 days ago",
+                    username: "a username"
+                )
+            ),
+            ImageCommentCellController(
+                model: ImageCommentViewModel(
+                    message: "nice",
+                    date: "1 hour ago",
+                    username: "a."
+                )
+            ),
+        ]
+    }
+}
