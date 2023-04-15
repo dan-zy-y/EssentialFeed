b3f74b60a3e1c3634afca3643b8a996e74e227b4
Daniil Zadorozhnyy <dz@ditio.no>
2023-04-15 18:23:56 +0200

    Add FeedEndpoint with `after_id` param

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -83,7 +83,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
     
     private func makeRemoteFeedLoaderWithLocalFallback() -> AnyPublisher<Paginated<FeedImage>, Error> {
-        let url = FeedImagesEndpoint.get.url(baseURL: baseURL)
+        let url = FeedImagesEndpoint.get().url(baseURL: baseURL)
         
         return httpClient
             .getPublisher(url: url)
--- EssentialFeed/EssentialFeedTests/FeedAPI/FeedEndpointTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedAPI/FeedEndpointTests.swift
@@ -13,11 +13,24 @@ final class FeedEndpointTests: XCTestCase {
     func test_feed_endpointURL() {
         let baseURL = URL(string: "http://base-url.com")!
         
-        let received = FeedImagesEndpoint.get.url(baseURL: baseURL)
-        
+        let received = FeedImagesEndpoint.get().url(baseURL: baseURL)
+         
         XCTAssertEqual(received.scheme, "http", "scheme")
         XCTAssertEqual(received.host, "base-url.com", "host")
         XCTAssertEqual(received.path, "/feed", "path")
         XCTAssertEqual(received.query, "limit=10", "query")
+    }
+    
+    func test_feed_endpointURLAfterGivenImage() {
+        let image = uniqueImage() 
+        let baseURL = URL(string: "http://base-url.com")!
+        
+        let received = FeedImagesEndpoint.get(after: image).url(baseURL: baseURL)
+         
+        XCTAssertEqual(received.scheme, "http", "scheme")
+        XCTAssertEqual(received.host, "base-url.com", "host")
+        XCTAssertEqual(received.path, "/feed", "path")
+        XCTAssertEqual(received.query?.contains("limit=10"), true, "limit query")
+        XCTAssertEqual(received.query?.contains("after_id=\(image.id.uuidString)"), true, "after id query")
     }
 }
